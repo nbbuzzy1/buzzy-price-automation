034ae7dd0671a8e6c56cc586a8c366ab28ee5650
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-10-03 08:10:22 -0400

    Separate logic into functions; add auction item url; add timer.

--- automation.py
+++ automation.py
@@ -1,26 +1,44 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from threading import Timer
+import time
+
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
 
 
 def buzzy_price_automation():
-	chrome_browser = webdriver.Chrome('./chromedriver.exe')
-
+	chrome_browser = webdriver.Chrome('./chromedriver.exe', options=options)
 	chrome_browser.get('https://buzzyprice.com')
-
+	time.sleep(20)
+	buzzy_price_page_interaction(chrome_browser)
 	chrome_browser.get('https://google.com')
-
-	google_search_input = chrome_browser.find_element_by_class_name('gLFyf')
-	google_search_input.send_keys('buzzyprice ebay')
-	google_search_input.send_keys(Keys.ENTER)
-	buzzycite_link = chrome_browser.find_element_by_xpath("//h3[contains(text(),'Buzzy Price')]")
-
-	buzzycite_link.click()
-
+	google_page_interaction(chrome_browser)
+	time.sleep(20)
+	buzzy_price_page_interaction(chrome_browser)
 	chrome_browser.close()
+	print('successful automation')
 
-buzzy_price_automation()
-# def buzzycite_interval():
-#     Timer(10000.0, buzzycite_automation).start()
 
-# Timer(1.0, buzzycite_automation).start()
+def buzzy_price_page_interaction(chrome_browser):
+	auction_item_input = chrome_browser.find_element_by_id('auctionLink')
+	auction_item_url = 'https://www.ebay.com/itm/Playstation-5-Disc-Version-PS5/293764734938?hash=item4465be1fda:g:r3kAAOSwkplfa9Tn'
+	auction_item_input.send_keys(auction_item_url)
+	time.sleep(20)
+	auction_item_input.send_keys(Keys.ENTER)
+	time.sleep(75)
+
+
+def google_page_interaction(chrome_browser):
+	google_search_input = chrome_browser.find_element_by_class_name('gLFyf')
+	google_search_input.send_keys('buzzyprice ebay alert')
+	google_search_input.send_keys(Keys.ENTER)
+	time.sleep(35)
+	buzzy_price_url = 'https://www.buzzyprice.com/'
+	buzzy_price_link = chrome_browser.find_element_by_xpath('//a[@href="'+ buzzy_price_url +'"]')
+	buzzy_price_link.click()
+
+
+while True:
+	buzzy_price_automation()
+	time.sleep(10800)
+
